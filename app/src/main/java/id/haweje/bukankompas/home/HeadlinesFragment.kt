cc1fbaa87aa504559997ec1e1011e36e7600810e
hendrawanwib <hendrawanwib@gmail.com>
2021-08-28 14:28:27 +0700

    Have Bookmarked Feature

--- app/src/main/java/id/haweje/bukankompas/home/HeadlinesFragment.kt
+++ app/src/main/java/id/haweje/bukankompas/home/HeadlinesFragment.kt
@@ -1,4 +1,4 @@
-package id.haweje.bukankompas.home.technology
+package id.haweje.bukankompas.home.headlines
 
 import android.content.Intent
 import android.os.Bundle
@@ -8,20 +8,20 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.haweje.bukankompas.R
-import id.haweje.bukankompas.core.data.source.local.entity.TechEntity
-import id.haweje.bukankompas.core.ui.TechAdapter
+import id.haweje.bukankompas.core.data.source.local.entity.LocalNewsEntity
+import id.haweje.bukankompas.core.ui.ListAdapter
+import id.haweje.bukankompas.core.ui.OnItemClickNewsCallback
 import id.haweje.bukankompas.core.utils.ViewModelFactory
 import id.haweje.bukankompas.core.vo.Status
-import id.haweje.bukankompas.databinding.FragmentTechBinding
+import id.haweje.bukankompas.databinding.FragmentHeadlinesBinding
 import id.haweje.bukankompas.detail.DetailActivity
 import id.haweje.bukankompas.home.NewsViewModel
 
+class HeadlinesFragment : Fragment(R.layout.fragment_headlines) {
 
-class TechFragment : Fragment(R.layout.fragment_tech) {
-
-    private var _binding : FragmentTechBinding? = null
+    private var _binding : FragmentHeadlinesBinding? = null
     private lateinit var newsViewModel: NewsViewModel
-    private lateinit var techAdapter: TechAdapter
+    private lateinit var listAdapter: ListAdapter
     private val binding get() = _binding
 
     override fun onDestroyView() {
@@ -32,11 +32,12 @@ class TechFragment : Fragment(R.layout.fragment_tech) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        _binding = FragmentTechBinding.bind(view)
+        _binding = FragmentHeadlinesBinding.bind(view)
 
-        techAdapter = TechAdapter()
-        techAdapter.setOnItemClick(object : TechAdapter.OnItemClickNewsCallback {
-            override fun onItemClickedNews(news: TechEntity) {
+        listAdapter = ListAdapter()
+        listAdapter.notifyDataSetChanged()
+        listAdapter.setOnItemClick(object : OnItemClickNewsCallback{
+            override fun onItemClickedNews(news: LocalNewsEntity) {
                 Intent(activity, DetailActivity::class.java).also{
                     it.putExtra(DetailActivity.EXTRA_TITLE, news.title)
                     it.putExtra(DetailActivity.EXTRA_DESC, news.description)
@@ -46,29 +47,28 @@ class TechFragment : Fragment(R.layout.fragment_tech) {
                     it.putExtra(DetailActivity.EXTRA_CONTENT, news.content)
                     it.putExtra(DetailActivity.EXTRA_URL, news.url)
                     it.putExtra(DetailActivity.EXTRA_ID, news.id)
+
                     startActivity(it)
                 }
             }
         })
 
-
         binding?.apply {
             rvNews.layoutManager = LinearLayoutManager(view.context)
-            rvNews.adapter = techAdapter
+            rvNews.adapter = listAdapter
             rvNews.setHasFixedSize(true)
         }
 
-        showLoading(true)
         val factory = ViewModelFactory.getInstance(requireContext())
         newsViewModel = ViewModelProvider(this, factory)[NewsViewModel::class.java]
-        newsViewModel.getIndonesiaTechNews().observe(viewLifecycleOwner,{
+        newsViewModel.getIndonesiaNews().observe(viewLifecycleOwner, {
             if (it != null){
                 when(it.status){
                     Status.LOADING -> showLoading(true)
                     Status.SUCCESS ->{
                         showLoading(false)
-                        it.data?.let { news -> techAdapter.setNews(news) }
-                        techAdapter.notifyDataSetChanged()
+                        it.data?.let { news -> listAdapter.setNews(news) }
+                        listAdapter.notifyDataSetChanged()
                     }
                     Status.ERROR ->{
                         showLoading(false)
@@ -76,7 +76,6 @@ class TechFragment : Fragment(R.layout.fragment_tech) {
                     }
                 }
             }
-
         })
     }
 
@@ -87,4 +86,5 @@ class TechFragment : Fragment(R.layout.fragment_tech) {
             binding?.progressBar?.visibility = View.GONE
         }
     }
+
 }
